2c91ed67d8354c33110a2fe8b1ae827da22d48bf
rezerbit <rezerbit@gmail.com>
2018-11-21 15:22:34 +0500

    Add rspec

---
+++ spec/lib/nmax_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Nmax do
+  it 'has a version number' do
+    expect(Nmax::VERSION).not_to be nil
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/'
+
+  minimum_coverage 90
+end
+
+require 'nmax' # ATTENTION: require after simplecov
+
+RSpec.configure do |config|
+  config.filter_run_including(focus: true)
+  config.run_all_when_everything_filtered = true
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+end
