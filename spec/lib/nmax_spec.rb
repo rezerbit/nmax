2120ee9580b4afe0c3273cd33e5eada45c0a37df
rezerbit <rezerbit@gmail.com>
2018-11-21 18:34:11 +0500

    [Spec] Add describes for acceptance and load tests

--- spec/lib/nmax_spec.rb
+++ spec/lib/nmax_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe Nmax do
   it 'has a version number' do
     expect(Nmax::VERSION).not_to be nil
   end
+
+  describe 'Acceptance tests' do
+    context 'when a small input stream' do
+    end
+
+    context 'when a large input stream' do
+    end
+
+    context 'when the N argument is invalid' do
+      context 'when the argument is absent' do
+      end
+
+      context 'when the argument is zero' do
+      end
+
+      context 'when the argument is string' do
+      end
+    end
+  end
+
+  xdescribe 'Load test (Random generated stream)' do
+    pending 'Disabled because the test takes a long time'
+
+    context 'when the N argument is large' do
+    end
+  end
 end
